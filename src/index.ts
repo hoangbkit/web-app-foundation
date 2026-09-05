4409b1b9e28e1dc1feab103ce8022b02692c8b0e
Hoang Nguyen <hoangbkit@gmail.com>
2026-09-05 14:27:31 +0700

    Export analytics foundation API

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export {
+  BrowserAnalytics,
+  DEFAULT_ANALYTICS_ENDPOINT,
+  createBrowserAnalytics,
+} from "./analytics.js";
+
+export type {
+  AnalyticsBatch,
+  AnalyticsDaySnapshot,
+  AnalyticsEventCounter,
+  AnalyticsRuntime,
+  BrowserAnalyticsConfig,
+} from "./analytics.js";
